b592854957c495b0171ee4916ed75503cff9a06b
leonardo-merlo <leomamerlo@gmail.com>
2025-09-10 16:05:40 -0300

    fix: config

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('API rodando!');
+});
